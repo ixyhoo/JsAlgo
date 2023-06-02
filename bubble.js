0b5919549f51b099b1d5e1c01fd05ce84436aedb
ixyhoo <ixyhoo@outlook.fr>
2023-06-02 14:14:32 +0200

    sort methode

--- bubble.js
+++ bubble.js
@@ -1,16 +1,17 @@
 
-function bubbleSort(array) {
+/*function bubbleSort(array) {
   for (let i = 0; i < array.length -1; i++) {
     for (let j = 0; j < (array.length -1 - i); j++) {
       if (array[j] > array[j + 1]) {
-        let temp = array[j]
-        array[j] = array[j + 1]
-        array[j + 1] = temp
+      //  let temp = array[j]
+       // array[j] = array[j + 1]
+       // array[j + 1] = temp;
+       [array[j], array[j + 1]] = [array[j + 1], array[j]
         console.log(array);
       }
     }
   }
-  console.log(array)
-}
+  console.log(array);
+}*/
 
 bubbleSort([400, 2, 132, 47, 32, 6, 18, 5, 3]) 
